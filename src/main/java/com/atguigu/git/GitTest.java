6ec502d3f282734453e950fa8573d64aa70ae844
LiuXin <2697685513@qq.com>
2022-09-06 14:34:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author LiuXin
+ * @create 2022-09-06-14:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
